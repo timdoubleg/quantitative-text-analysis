d5836561d07446b7bd73b71b08ff76c7457e521d
dave <david.klug03@gmail.com>
2021-05-15 14:47:33 +0200

    Update 02_DataExploration.R

--- 02_DataExploration.R
+++ 02_DataExploration.R
@@ -13,31 +13,26 @@
 
 library(here)
 library(readtext)
+library(quanteda)
+library(stringr)
 Sys.setenv(lang = "ENG")
 
 # 2 Data import ----
 #===================#
 
-#the code below should help finding corrupt files by looping through all PDFs in the list
-df_test <- data.frame() #creating an empty dataframe to be filled 
-for(file in list.files(here("data"))) {
-  loaded_text <- readtext(here("data", file), docvarsfrom = "filenames")
-  df_test <- rbind(df_test, loaded_text)
-} 
-
 df1 <- readtext(here("data"),
          docvarsfrom = "filenames") 
 #The filename correspond with the federal register doc id. They do not provide additional information, thus no further docvars are specified.
 
-
-
-## 2.1 Data import: notes ----
-#===================#
-
 # Some PDFs like 08-62.pdf contain extensive parts with scanned text.
+
+corp1 <- corpus(df1)
 
 # 3 Data cleaning ----
 #===================#
+
+## Finding the date of issuance through regex ----
+
 
 
 # 4 Data exploration ----
