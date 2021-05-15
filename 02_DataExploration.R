3bdd0eaed2944768f572e4151663e5ae559c8b4c
dave <david.klug03@gmail.com>
2021-05-15 14:23:48 +0200

    finding corrupt PDFs through loop

--- 02_DataExploration.R
+++ 02_DataExploration.R
@@ -13,19 +13,28 @@
 
 library(here)
 library(readtext)
-library(dplyr)
 Sys.setenv(lang = "ENG")
 
 # 2 Data import ----
 #===================#
 
+#the code below should help finding corrupt files by looping through all PDFs in the list
+df_test <- data.frame() #creating an empty dataframe to be filled 
+for(file in list.files(here("data"))) {
+  loaded_text <- readtext(here("data", file), docvarsfrom = "filenames")
+  df_test <- rbind(df_test, loaded_text)
+} 
+
 df1 <- readtext(here("data"),
          docvarsfrom = "filenames") 
 #The filename correspond with the federal register doc id. They do not provide additional information, thus no further docvars are specified.
-# Some PDFs like 08-62.pdf contain extensive parts of scanned text. This may have to be taken into account during the analysis or interpretation of the results.
-#Two files had to be manually deleted.
 
 
+
+## 2.1 Data import: notes ----
+#===================#
+
+# Some PDFs like 08-62.pdf contain extensive parts with scanned text.
 
 # 3 Data cleaning ----
 #===================#
