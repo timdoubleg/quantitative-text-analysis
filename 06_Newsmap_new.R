4fe9dba8aab0b6a83c8d1294c79aae2b41c609c4
new-s-22 <66666596+new-s-22@users.noreply.github.com>
2021-05-23 14:03:32 +0200

    Trying to solve Newsmap

--- 06_Newsmap_new.R
+++ 06_Newsmap_new.R
@@ -28,13 +28,21 @@ library(tm) #text mining
 library(circlize) #already loaded, but just being comprehensive
 library(plotly) #interactive ggplot graphs
 library(stm)
+library(here)
+library(readtext)
+library(stringr)
+library(newsmap)
+library(sentimentr)
+library(tidyr)
+library(maps)
+library(countrycode)
 
 rm(list=ls())
 
 # set wd to where file is
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-data <- fread('./data/executive_orders_withcountry.csv')
+data <- fread('./data/executive_orders_cleaned.csv')
 
 # 2 Topic Modeling wtih Lexicoder ----
 #===================#
@@ -89,15 +97,146 @@ ggplot(topics.df, aes(x="", y = sum, fill = topic)) +
 
 # # compute the most relevant per document
 topics.lexicoder <- convert(eo.dfm, to = 'data.frame')
-topics.lexicoder <- topics.lexicoder %>% gather(key = 'topic', value = 'count', -doc_id)
 length(unique(topics.lexicoder$doc_id))
 
+highest_topic_df <- subset(topics.lexicoder, select = -c(doc_id) )
+highest_topic <- colnames(highest_topic_df)[max.col(highest_topic_df,ties.method="first")]
 
-data<-cbind(data,topics.lexicoder)
+data<-cbind(data,highest_topic)
 
 
 
-# 
-topics.lexicoder2 <- topics.lexicoder %>% group_by(doc_id) %>% top_n(1, count) 
-nrow(topics.lexicoder2) # we have some documents, for which we have equal counts
-length(unique(topics.lexicoder2$doc_id))
+# Newsmap
+target_topics <- c('macroeconomics', 'foreign_trade','intl_affairs','defence','culture','sstc','finance','civil_rights')
+data <- filter(data, highest_topic %in% target_topics)
+
+eo.corpus <- corpus(data, 
+                    docid_field =  "eo_number", 
+                    text_field = 'text')
+head(summary(eo.corpus))
+
+# set some dictionaries for later
+month <- c("January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December")
+day <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday")
+USA <- c("United","America","American","Americans","Washington")
+
+# create tokens
+eo.tokens <- tokens(eo.corpus, 
+                    remove_punct = TRUE,
+                    remove_numbers = TRUE,
+                    remove_symbols = TRUE) %>%
+  tokens_remove(c(stopwords("english"),
+                  month,
+                  day,
+                  USA)) 
+
+# create labels
+eo.label <- tokens_lookup(eo.tokens, 
+                          dictionary = data_dictionary_newsmap_en, 
+                          levels = 3) # level 3 stands for countries
+
+# Document-feature matrix
+dfmat_label <- dfm(eo.label, tolower = FALSE)
+dfmat_feat <- dfm(eo.tokens, tolower = FALSE)
+
+# select 
+dfmat_feat_select <- dfm_select(dfmat_feat, pattern = "^[A-Z][A-Za-z0-9]+", 
+                                valuetype = "regex", case_insensitive = FALSE) %>% 
+  dfm_trim(min_termfreq = 10)
+
+# train the newsmap textmodel
+tmod_nm <- textmodel_newsmap(dfmat_feat_select, y = dfmat_label) 
+
+# check which coefficients are associated to the individual countries
+coef(tmod_nm,n=15)[c("US","CN","IQ", "IN")] 
+
+# predict and cluster country labels on our documents
+pred_nm <- predict(tmod_nm)
+count <-table(pred_nm) 
+count
+
+# add country to dataframe
+data$iso <- pred_nm
+data$country <- countrycode(pred_nm, origin = 'iso2c', destination = 'country.name')
+
+
+# plot the newsmap
+dat_country <- as.data.frame(count, stringsAsFactors = FALSE)
+dat_country <- dat_country[order(-dat_country$Freq),]
+dat_country$country <- countrycode(dat_country$pred_nm, origin = 'iso2c', destination = 'country.name')
+colnames(dat_country) <- c("id", "frequency")
+world_map <- map_data(map = "world")
+world_map$region <- iso.alpha(world_map$region) # convert country name to ISO code
+
+plot.map <- ggplot(dat_country, aes(map_id = id)) +
+  geom_map(aes(fill = frequency), map = world_map) +
+  expand_limits(x = world_map$long, y = world_map$lat) +
+  scale_fill_continuous(name = "Frequency") +
+  theme_void() +
+  coord_fixed() +
+  scale_fill_gradientn(colors=c("#56B1F7","green","yellow","orange","#ff0000"), values = scales::rescale(c(5, 25, 100, 200, 400))) +
+  labs(title = 'Frequency of countries (1950 -2021)', 
+       subtitle = paste0('n = ', nrow(data)))
+plot.map
+
+# check for counts of EU
+target <- c('Germany', 'France', 'Italy', 'United Kingdom', 'Belgium', 'Poland', 'Sweden')
+EU <- filter(dat_country, country %in% target)
+sum(EU$Freq)
+
+# check Top 10 countries and convert ISO
+top10 <- dat_country[1:10, ]
+top10$country <- countrycode(top10$id, origin = 'iso2c', destination = 'country.name')
+top10 <- top10[order(-top10$frequency),]
+rownames(top10) <- NULL
+
+# get EOs only for top 10 countries
+target <- top10$country
+eo.top10 <- filter(data, country %in% target)
+nrow(eo.top10)/nrow(data) # account for the majority
+
+# plot frequency of top 10 countries
+plot.top10 <- ggplot(top10, aes(x = frequency, y = reorder(country, frequency))) +
+  geom_bar(stat = 'identity') + 
+  labs(title = 'Top 10 Frequency of countries (1950 -2021)', 
+       y = '',
+       x = 'number of EOs',
+       subtitle = paste0('n = ', nrow(eo.top10))
+  ) +
+  theme(plot.subtitle=element_text(size=9, hjust=0, face="italic", color="black"))
+plot.top10
+
+
+# plot counts over times
+eo.top10$year <- year(eo.top10$date)
+
+# count EOs per year and country
+country.long <- eo.top10 %>% count(year, country)
+
+# plot top 10 over time
+plot.top10.time <- ggplot(country.long, aes(x=year, y=n, color = factor(country))) + 
+  geom_line() +
+  facet_grid(rows = vars(reorder(country, -n)), scales = 'free') +
+  labs(title = 'Top 10 EOs counts over time (1950 -2021)', 
+       y = '',
+       x = 'Years',
+       subtitle = paste0('n = ', nrow(eo.top10))
+  ) +
+  theme(plot.subtitle=element_text(size=9, hjust=0, face="italic", color="black")) +
+  theme_bw() + 
+  theme(legend.position = "none") 
+plot.top10.time
+
+# with fixed
+plot.top10.time <- ggplot(country.long, aes(x=year, y=n, color = factor(country))) + 
+  geom_line() +
+  facet_grid(rows = vars(reorder(country, -n)), scales = 'fixed') +
+  labs(title = 'Top 10 EOs counts over time (1950 -2021)', 
+       y = '',
+       x = 'Years',
+       subtitle = paste0('n = ', nrow(eo.top10))
+  ) +
+  theme(plot.subtitle=element_text(size=9, hjust=0, face="italic", color="black")) +
+  theme_bw() + 
+  theme(legend.position = "none") 
+plot.top10.time
